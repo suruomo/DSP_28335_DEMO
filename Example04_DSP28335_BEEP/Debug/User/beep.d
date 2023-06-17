d8af0915b6b1059362cb052318e23d3235718b61
suruomo <www.suruomo@hotmail.com>
2023-06-17 22:47:07 +0800

    蜂鸣器

---
+++ Example04_DSP28335_BEEP/Debug/User/beep.d
@@ -0,0 +1,51 @@
+# FIXED
+
+User/beep.obj: ../User/beep.c
+User/beep.obj: ../User/beep.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Device.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Adc.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_DevEmu.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_CpuTimers.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_ECan.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_ECap.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_DMA.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_EPwm.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_EQep.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Gpio.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_I2c.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Mcbsp.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_PieCtrl.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_PieVect.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Spi.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Sci.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_SysCtrl.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_XIntrupt.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Xintf.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Examples.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_GlobalPrototypes.h
+User/beep.obj: D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_DefaultISR.h
+
+../User/beep.c: 
+../User/beep.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Device.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Adc.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_DevEmu.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_CpuTimers.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_ECan.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_ECap.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_DMA.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_EPwm.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_EQep.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Gpio.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_I2c.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Mcbsp.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_PieCtrl.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_PieVect.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Spi.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Sci.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_SysCtrl.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_XIntrupt.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Xintf.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_Examples.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_GlobalPrototypes.h: 
+D:/00\ Project/DSP_28335_PZ/WorkSpace_CCS9.0_28335_Practice/Example04_DSP28335_BEEP/Include/DSP2833x_DefaultISR.h: 
